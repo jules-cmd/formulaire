7314ac145165dfb7af5c8eb5335d0f2d15cd115e
jules-cmd <jules.mijon@gmail.com>
2022-04-13 16:22:35 +0200

    first commit

---
+++ js/connexion.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log(200);
+    
+//recupérer les valeurs dans "index.html"
+    const user = {
+        nom: document.getElementsByTagName("input")[0].value,
+        prenom: document.getElementsByTagName("input")[1].value,
+        validate: document.getElementsByTagName("input")[2],
+        getOut() {
+
+            return this.validate;
+        }
+    };
+    let prints = user.getOut();
+
+    user.validate.addEventListener("click", e => {
+        e.preventDefault();
+        console.log(prints);
+    });
+});
